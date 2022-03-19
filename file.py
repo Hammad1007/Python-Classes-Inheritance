d5a062da02bee3d60a42bd5bf67cd1213de0e705
Hammad Rashid (Hammad) <76726810+Hammad1007@users.noreply.github.com>
2022-03-19 21:26:46 +0500

    Create file.py

---
+++ file.py
@@ -0,0 +1,32 @@
+# Task 4
+# Inheritance and Classes
+class Rectangle:
+  Area = 0.00;
+  def __init__(self, len, wid):
+   self.len = len;
+   self.wid = wid;
+
+  def find_Area(self):
+    self.Area = self.len * self.wid;
+
+  def print_Area(self):
+    print('The area is: ', self.Area);
+
+class Trapezium(Rectangle):
+  def __init__(self, len, wid, h):
+    super().__init__(len, wid);
+    self.h = h; # height of the trapezium
+
+  def find_Area(self):
+    self.Area = 0.5 * (self.len + self.wid) * self.h;
+
+  def print_Area(self):
+    print("The area of trapezium is:", self.Area);
+
+r1 = Rectangle(10, 8);
+r1.find_Area();
+r1.print_Area();
+
+t1 = Trapezium(10, 8, 5);
+t1.find_Area();
+t1.print_Area();
